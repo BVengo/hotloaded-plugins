0c8dc2cbf46a40c4d86f65bda0f44dd843b6a547
Benjamin van de Vorstenbosch <bencvdv@gmail.com>
2024-02-06 11:05:34 +1100

    Test - add and modify plugins

--- hotloaded_plugins/plugins/plugin2.py
+++ hotloaded_plugins/plugins/plugin2.py
@@ -1,6 +1,6 @@
 from .base_plugin import BasePlugin
 
 
-class Plugin1(BasePlugin):
+class Plugin2(BasePlugin):
     def get_description(self) -> str:
-        return "Changed plugin 1 description!"
+        return "Added a new plugin 2!"

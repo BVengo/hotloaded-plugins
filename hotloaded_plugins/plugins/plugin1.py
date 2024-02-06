db447c52a7720c9bc0f5e187c17a10bfc2c04b5c
Benjamin van de Vorstenbosch <bencvdv@gmail.com>
2024-02-06 11:13:40 +1100

    Test - delete and modify plugins

--- hotloaded_plugins/plugins/plugin1.py
+++ hotloaded_plugins/plugins/plugin1.py
@@ -3,4 +3,4 @@ from .base_plugin import BasePlugin
 
 class Plugin1(BasePlugin):
     def get_description(self) -> str:
-        return "Changed plugin 1 description!"
+        return "This is plugin 1."
--- hotloaded_plugins/plugins/plugin2.py
+++
@@ -1,6 +0,0 @@
-from .base_plugin import BasePlugin
-
-
-class Plugin2(BasePlugin):
-    def get_description(self) -> str:
-        return "Added a new plugin 2!"
